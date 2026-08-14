163cdbdbc87ceca9f9341d3e2486461bf45c4e54
primekeibel-oss <prime.keibel@gmail.com>
2026-08-13 20:32:32 -0400

    Update python.py
    esta pro

--- python.py
+++ python.py
@@ -1,1 +1,20 @@
-print("hola")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL":"Una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado",
+    
+            }
+while True:
+    print("...")
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+        # ¿Qué debemos hacer si se encuentra la palabra?
+    else:
+        print("no la encontre")
+    
+        # ¿Qué hacer si no se encuentra la palabra?
